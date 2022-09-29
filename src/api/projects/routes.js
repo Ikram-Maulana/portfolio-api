7620184e07cef73e4ee8277b1f4bf452e7d62ab9
ikram-maulana <ikram075@ummi.ac.id>
2022-09-29 19:55:47 +0700

    Feat(Auth): Authentication Implemented Part 2

--- src/api/projects/routes.js
+++ src/api/projects/routes.js
@@ -3,6 +3,9 @@ const routes = (handler) => [
     method: "POST",
     path: "/projects",
     handler: handler.postProjectHandler,
+    options: {
+      auth: "portfolioapp_jwt",
+    },
   },
   {
     method: "GET",
@@ -13,11 +16,17 @@ const routes = (handler) => [
     method: "PUT",
     path: "/projects/{id}",
     handler: handler.putProjectByIdHandler,
+    options: {
+      auth: "portfolioapp_jwt",
+    },
   },
   {
     method: "DELETE",
     path: "/projects/{id}",
     handler: handler.deleteProjectByIdHandler,
+    options: {
+      auth: "portfolioapp_jwt",
+    },
   },
 ];
 
