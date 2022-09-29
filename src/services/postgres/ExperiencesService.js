5b2c86f5f8ecb9011035711422a846830c0249a5
ikram-maulana <ikram075@ummi.ac.id>
2022-09-29 14:36:29 +0700

    Feat(Database): Make addExperience Function

---
+++ src/services/postgres/ExperiencesService.js
@@ -0,0 +1,28 @@
+const { nanoid } = require("nanoid");
+const { Pool } = require("pg");
+const InvariantError = require("../../exceptions/InvariantError");
+
+class ExperiencesService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addExperience({ period, position, description }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: "INSERT INTO experiences VALUES($1, $2, $3, $4, $5, $6) RETURNING id",
+      values: [id, period, position, description, createdAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError("Experience gagal ditambahkan");
+    }
+
+    return result.rows[0].id;
+  }
+}
