fcd74ff5a43b55bbb4c5f3b7869600ea6f5bb396
ikram-maulana <ikram075@ummi.ac.id>
2022-09-29 16:31:54 +0700

    Feat(Database): Make Table with Migrate Technique

---
+++ migrations/1664443554177_create-table-projects.js
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("projects", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    name: {
+      type: "TEXT",
+      notNull: true,
+    },
+    image: {
+      type: "TEXT",
+      notNull: true,
+    },
+    tech: {
+      type: "TEXT[]",
+      notNull: true,
+    },
+    github_link: {
+      type: "TEXT",
+      notNull: true,
+    },
+    demo_link: {
+      type: "TEXT",
+      notNull: true,
+    },
+    created_at: {
+      type: "TEXT",
+      notNull: true,
+    },
+    updated_at: {
+      type: "TEXT",
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("projects");
+};
